1bdb0b95587ebb1c35f28e7c1bb218b967c675ee
Martin Parizek <marti.parizek@gmail.com>
2022-08-30 00:39:43 +0200

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+import useStyles from './styles';
 
-function App() {
+import { NavBar, Movies, Profile, MovieInformation, Actors } from '.';
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      Hello World
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+        </Routes>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MovieInformation = () => {
+  console.log('MovieInformation');
+  return (
+    <div>Movie Information</div>
+  );
+};
+
+export default MovieInformation;
